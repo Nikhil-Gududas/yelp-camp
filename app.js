b9483e5915b995d619f212cbad24f3aa811d8292
Nikhil Gududas <nikhil001.dev@gmail.com>
2022-08-04 20:56:27 +0530

    set ejs as view engine and home page created

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 
-
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.send('Home');
+    res.render('home');
 })
 
 
