f14f8b7ee6f2208cd7e348044fb5ed7b6398be5c
Nikhil Gududas <nikhil001.dev@gmail.com>
2022-08-04 21:15:07 +0530

    added database connection in app.js

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
 
+//connectin the mongodb database using mongoose
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error:")); //showing error if there is any error while connecting database
+db.once("open", () => {
+    console.log("Database Connected") //showing successess message if database connected successfully
+})
+
+app.set('view engine', 'ejs'); //setting ejs as view engine
+app.set('views', path.join(__dirname, 'views'));  //setting the path of view folder
 
 app.get('/', (req, res) => {
     res.render('home');
