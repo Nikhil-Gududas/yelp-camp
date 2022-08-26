ab3df217f537065af6d9ac8c9f2226cc8b4de280
Nikhil-Gududas <nikhil001.dev@gmail.com>
2022-08-26 11:03:17 +0530

    validateForm moved to javascript

--- routes/users.js
+++ routes/users.js
@@ -5,20 +5,16 @@ const router = express.Router();
 const users = require('../controllers/users');
 const catchAsync = require('../utils/catchAsync')
 
-router.get('/register', users.renderRegister);
-
-router.post('/register', catchAsync(users.registerUser));
-
-router.get('/login', users.renderLogin)
-
-router.post('/login',
-    passport.authenticate('local',
-        {
-            failureFlash: true,
-            failureRedirect: '/login',
-            keepSessionInfo: true,
-        }),
-    users.login)
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.registerUser));
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', {
+        failureFlash: true,
+        failureRedirect: '/login',
+        keepSessionInfo: true
+    }), users.login)
 
 router.get('/logout', users.logout)
 
